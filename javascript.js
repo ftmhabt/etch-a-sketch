da60b79869a913bc3e8cb15eafcc31571bd62c37
ftmhazd <149063046+ftmhazd@users.noreply.github.com>
2023-12-17 23:04:11 +0330

    Make 16 * 16 grid

---
+++ javascript.js
@@ -0,0 +1,22 @@
+let body=document.querySelector('body');
+let container=document.createElement('div');
+container.classList.add('container');
+
+body.appendChild(container);
+
+for (let i = 0; i < 16; i++) {
+
+    let gridRow=document.createElement('div');
+    gridRow.classList.add('grid-row');
+    
+
+    for (let j = 0; j < 16; j++) {
+        let div=document.createElement('div');
+        div.classList.add('grid-item');
+
+        gridRow.appendChild(div);
+    }
+
+    container.appendChild(gridRow);
+
+}
